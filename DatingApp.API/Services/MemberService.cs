6dd1d074d9598dcdf3483783edab089c7b450852
QuangTan186 <83267682+QuangTan186@users.noreply.github.com>
2022-10-30 09:28:35 +0700

    success

---
+++ DatingApp.API/Services/MemberService.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.DTOs;
+
+namespace DatingApp.API.Services
+{
+    public class MemberService : IMemberService
+    {
+        private readonly DataContext _context;
+
+        public MemberService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public MemberDto GetMemberByUsernam(string Username)
+        {
+            var user = _context.AppUsers.FirstOrDefault(x=> x.Username == Username)
+            if(user == null) return null;
+            return new MemberDto
+            {
+                Avatar = user.Avatar,
+                City = user.City,
+                CreateAt = user.CreateAt,
+                DateOfBirth = user.DateOfBirth,
+                Email = user.Email,
+                Gender = user.Gender,
+                Introduction = user.Introduction,
+                KnownAs = user.KnownAs,
+                Username = user.Username
+            };
+        }
+
+        public List<MemberDto> GetMembers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
